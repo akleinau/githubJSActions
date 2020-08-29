b28b269b37dcbbc5924c4742dcc2df163dbd7ce3
akleinau <56925445+akleinau@users.noreply.github.com>
2020-08-29 15:17:25 +0200

    add column move action

--- DiscussedToColumn/index.js
+++ DiscussedToColumn/index.js
@@ -3,7 +3,7 @@ require('cross-fetch/polyfill');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-fetch('https://api.github.com/repos/akleinau/test/issues/1/comments')
+fetch('https://api.github.com/repos/akleinau/githubJSActions/issues/1/comments')
   .then(res => {
     if (res.status >= 400) {
       throw new Error("Bad response from server");
@@ -11,7 +11,11 @@ fetch('https://api.github.com/repos/akleinau/test/issues/1/comments')
     return res.json();
   })
   .then(user => {
+    count = user.length
     console.log(user.length);
+    if (count > 4) core.setOutput("continue", true);
+    else core.setOutput("continue", false);
+
   })
   .catch(err => {
     console.error(err);
